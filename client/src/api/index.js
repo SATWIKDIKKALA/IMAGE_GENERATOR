b5b52eb58d116b179501bdd6cd5e09c1d4f052b4
SATWIKDIKKALA <43822762+SATWIKDIKKALA@users.noreply.github.com>
2025-03-02 20:05:08 -0500

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://image-generator-4l8k.onrender.com/api/",
+  baseURL: "https://image-generator-y1ch.onrender.com/api/",
 });
 
 export const GetPosts = async () => await API.get("/post/");
